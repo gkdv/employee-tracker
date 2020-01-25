1184a2e66a916eea3369b2ad88ba23ff1db45b3b
garrett krage <garrett.krage@gmail.com>
2020-01-25 16:27:49 -0700

    ...now there should be a packagejson.

---
+++ employee_db.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+title VARCHAR(50) NOT NULL,
+department VARCHAR(50) NOT NULL,
+salary INT,
+manager VARCHAR(50) NOT NULL
+);
+
+SELECT * FROM employee;
