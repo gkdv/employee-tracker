14a6fbc225f97c214aa38f74201ebd7856a739d6
garrett krage <garrett.krage@gmail.com>
2020-03-08 10:50:44 -0600

    added npm i packages and server.js at start folder.

--- index_db.js
+++ index_db.js
@@ -1,63 +1,78 @@
 
 const mysql = require('mysql');
+const fs = require('fs')
 
-
+// fs.readFile('employees.txt', (err, data) => {
+//   if (err) throw err;
+//   console.log(JSON.parse(data))
+//   // console.log(.last_name)
+//   console.log('----------------------------------')
+// })
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '5kgk4107.cu.du',
-    port: 3306,
-    database: 'employee_db'
+  host: 'localhost',
+  user: 'root',
+  password: '5kgk4107.cu.du',
+  port: 3306,
+  database: 'employee_db'
 });
 
 connection.connect(err => {
-    if (err) throw err;
-    console.log(`Connected as id ${connection.threadId}`);
-    createEmployee();
-    deleteEmployee();
-    viewAll();
+  if (err) throw err;
+  console.log(`Connected as id ${connection.threadId}`);
+  createEmployee();
+  // deleteEmployee();
+  viewAll();
 })
 
 function viewAll() {
-    connection.query("SELECT * FROM employee", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    });
-  }
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
 
-  function createEmployee() {
-    console.log('Inserting a new employee...\n');
-    var query = connection.query(
-        'INSERT INTO employee SET ?',
-        [{
-            first_name: 'garrett',
-            last_name: 'krage',
-            title: 'mySQL developer',
-            department: 'developement',
-            salary: '60000',
-            manager: 'Chris Farley'
-        }],
-        function (err, res){
-            if (err) throw err;
-            console.log(res.affectedRows + ' employee inserted!\n');
-            // updateProduct();
-        }
-    )
+function createEmployee() {
+  console.log('Inserting a new employee...\n');
+
+  fs.readFile('employees.txt', function(err, data){
+    if (err) throw err;
+    let person = JSON.parse(data);
+    console.log('this is the object parsed into createEmployee...' + person)
+    
+  })
+  connection.query(
+    'INSERT INTO employee SET ?', person,
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + ' employee inserted!\n');
+      // updateProduct();
+    }
+  )
 }
 
 function deleteEmployee() {
-    console.log("Deleting Employee...\n");
-    connection.query(
-      "DELETE FROM employee WHERE ?",
-      {
-        first_name: "garrett"
-      },
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " employee deleted!\n");
-      }
-    );
-  }
-  
+  console.log("Deleting Employee...\n");
+  connection.query(
+    "DELETE FROM employee WHERE ?",
+    {
+      first_name: "garrett"
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee deleted!\n");
+    }
+  );
+}
+
+
+// [{
+//   first_name: 'garrett',
+//   last_name: 'krage',
+//   title: 'mySQL developer',
+//   department: 'developement',
+//   salary: '60000',
+//   manager: 'Chris Farley'
+// }]
+
--- index_inquirer.js
+++ index_inquirer.js
@@ -1,8 +1,25 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
+const fs = require('fs');
 // const path = require('path');
-// const Employee = require('./constructors/employee_constructor');
+const mysql = require('mysql');
+const Employee = require('./constructors/employee_constructor');
+// const index_db = require('./index_db');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '5kgk4107.cu.du',
+    port: 3306,
+    database: 'employee_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+    createEmployee();
+    // deleteEmployee();
+    // viewAll();
+})
 
 // const employee1 = new Employee('sam', 'krage', 'software-guru', 'developement', 60000, 'mike drewer')
 // console.log(employee1)
@@ -67,6 +84,17 @@ function createEmployee() {
     ]).then(answers => {
         answers.employee_salary = parseInt(answers.employee_salary)
         console.log(answers)
+        // createEmployee(answers)
+        newPerson = new Employee(answers.first_name, answers.last_name, answers.job_title, answers.employee_department, answers.employee_salary, answers.employee_manager);
+        console.log(newPerson)
+
+        createEmployee(newPerson);
+
+        // fs.appendFile('employees.txt', JSON.stringify(answers) + '\n\n', (err) => {
+        //     if (err) throw err;
+        //     console.log('the file has been created and the new employee is inside...')
+        // })
+
     })
 }
 
@@ -74,5 +102,33 @@ starterPrompt();
 
 
 
+function createEmployee() {
+    console.log('Inserting a new employee...\n');
+
+    // fs.readFile('employees.txt', function (err, data) {
+    //     if (err) throw err;
+    //     let person = JSON.parse(data);
+    //     console.log('this is the object parsed into createEmployee...' + person)
+
+    // })
+
+    connection.query(
+        'INSERT INTO employee SET ?',
+        [],
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + ' employee inserted!\n');
+            // updateProduct();
+        }
+    )
+}
 
 
+// {
+//     first_name: 'garrett',
+//     last_name: 'matters',
+//     title: 'mySQL developer',
+//     department: 'developement',
+//     salary: '60000',
+//     manager: 'Chris Farley'
+// }
